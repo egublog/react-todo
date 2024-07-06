a6582b64a64fdbf3a3bedad2070986ec33b9ec60
egublog <egukei0729@gmail.com>
2024-07-06 23:25:14 +0900

    modify: ESLintとPrettierを統合して保存時に自動フォーマットを有効化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,16 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
   },
 };
